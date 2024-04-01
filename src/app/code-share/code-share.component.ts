200468cea61fcb266d7099c7ab088b38923877cd
Antonio Tornero <antoniotornero@hotmail.es>
2024-04-01 13:07:17 +0100

    adding llama button

--- src/app/code-share/code-share.component.ts
+++ src/app/code-share/code-share.component.ts
@@ -1,29 +1,28 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-interface ApiResponse {
-  result: string;
-}
-
 @Component({
   selector: 'app-code-share',
   templateUrl: './code-share.component.html',
   styleUrls: ['./code-share.component.css']
 })
 export class CodeShareComponent {
-  codeInput = '';
-  testInput = '';
-  codeOutput = '';
-  isLoading = false;  // Flag to indicate loading state
+  codeInput: string = '';
+  testInput: string = '';
+  codeOutput: string = '';
+  isLoading: boolean = false;
+  selectedApi: string = 'openai'; // Default selection
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   processCode(): void {
-    this.isLoading = true; // Start loading
-    const apiUrl = 'http://localhost:8080/api/submit';
-  
-    // Set responseType to 'text' to expect a plain text response
-    this.http.post(apiUrl, { code: this.codeInput, tests: this.testInput }, { responseType: 'text' })
+    this.isLoading = true;
+    this.codeOutput = ''; // Reset output
+
+    const endpointUrl = this.selectedApi === 'openai' ? 'http://localhost:8080/api/submit_gpt' : 'http://localhost:8080/api/submit_llama';
+
+        // Set responseType to 'text' to expect a plain text response
+    this.http.post(endpointUrl, { code: this.codeInput, tests: this.testInput }, { responseType: 'text' })
       .subscribe(response => {
         this.codeOutput = response;
         this.isLoading = false; // Stop loading once the response is received
@@ -32,6 +31,5 @@ export class CodeShareComponent {
         this.isLoading = false; // Stop loading in case of an error
       });
   }
-  
 }
 
